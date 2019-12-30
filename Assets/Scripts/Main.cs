35f9bf49c71b30b3a39b9696c7b7eca407bd6c17
doRA <dev9876dora@gmail.com>
2019-12-30 16:18:38 +0900

    [add] marching cubes table

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,20 @@
+﻿// Marching Cubes in Unity
+// Reference: https://github.com/Scrawk/Marching-Cubes
+
+using UnityEngine;
+
+namespace Arihara.MarchingCubesGPU
+{
+  public class Main : MonoBehaviour
+  {
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+  }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Marching-Cubes-GPU
 Implementation of Marching Cubes on GPU.
+
+Reference:  
+Marching Cubes in Unity : https://github.com/Scrawk/Marching-Cubes
